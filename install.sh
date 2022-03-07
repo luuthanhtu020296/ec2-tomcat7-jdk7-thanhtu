0cd60232d85cf3c8aa5f200ecad554a123bf8ecb
LUUTHANHTU <53068248+luuthanhtu@users.noreply.github.com>
2022-03-07 20:48:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,23 @@
+#!/bin/bash
 
-#
+wget https://files-cdn.liferay.com/mirrors/download.oracle.com/otn-pub/java/jdk/7u80-b15/jdk-7u80-linux-x64.tar.gz
+
+sudo mkdir /usr/lib/jvm
+
+sudo tar -C /usr/lib/jvm -xzf jdk-7u80-linux-x64.tar.gz
+
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.7.0_80/jre/bin/java 1
+
+wget https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.32/bin/apache-tomcat-7.0.32.tar.gz
+
+mkdir /opt/tomcat
+
+sudo tar -C /opt/tomcat xvzf apache-tomcat-7.0.32.tar.gz
+
+sudo echo "export JAVA_HOME=/usr/lib/jvm/jdk1.7.0_80/jre/bin/java" > ~/.bashrc
+
+sudo echo "CATALINA_HOME=/opt/tomcat/apache-tomcat-7.0.32" > ~/.bashrc
+
+. ~/.bashrc
+
+sudo $CATALINA_HOME/bin/startup.sh
